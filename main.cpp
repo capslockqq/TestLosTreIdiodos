fba9bb3afaff95fc3490ec832a161378fdb57fdc
capslcokqq <emil.ringgaard@hotmail.com>
2018-02-16 10:49:48 +0100

    added function for testing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
 using namespace std;
+void MadsFunktion();
+void AndersFunktions();
+void EmilsFunktion();
+
 
 int main() {
 	
 	cout << "hej" << endl;
 	return 0;
+}
+
+void MadsFunktion() {
+	
+}
+
+void AndersFunktions() {
+	
+}
+
+void EmilsFunktion() {
+	
 }
